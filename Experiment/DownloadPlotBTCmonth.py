12056b4359a0d386973280993de82cd472c3644f
DiogoMag <diogor.magalhaes@gmail.com>
2023-04-06 22:30:54 +0100

    1

--- Experiment/DownloadPlotBTCmonth.py
+++ Experiment/DownloadPlotBTCmonth.py
@@ -13,6 +13,8 @@ one_month_ago = (today - timedelta(days=30)).strftime('%Y-%m-%d')
 ## pull data from yahoo finance ( 1 month back )
 data = yf.download("BTC-USD", start=one_month_ago, end=today)
 
+data = data.ta.macd()
+
 ## these define what the graph will look like
 ## 'Date' not found in the df, because it was in the index, not a column. So we need to add it
 data.plot(y='Adj Close')
