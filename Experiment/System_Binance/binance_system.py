488cc4a9f499639054c40530da89e0a2fc76fee9
DiogoMag <diogor.magalhaes@gmail.com>
2023-04-09 20:14:05 +0100

    Added: get_server_status func

--- Experiment/System_Binance/binance_system.py
+++ Experiment/System_Binance/binance_system.py
@@ -1,16 +1,27 @@
-import datetime
 from binance import Client
+import os
+import datetime
+
+# Clear screen
+os.system('cls')
+
+# Get client Keys form system enviroment (Leave # at the beggining for the one you don't want to use)
+#client = Client(os.environ.get('API_KEY'), os.environ.get('API_SKEY')) #General
+client = Client(os.environ.get('TEST_API'), os.environ.get('TEST_SAPI'), testnet=True) #Testnet
 
 
 # Display server time and status
-server_status = Client.get_system_status()
-server_status = server_status["msg"]
-server_time = Client.get_server_time()
-server_time = server_time["serverTime"]
-server_time = datetime.datetime.fromtimestamp(server_time/1000)
-server_time = server_time.replace(second=0, microsecond=0)
-server_time = server_time.strftime("%H:%M %d-%m-%Y")
-print("Server status is " + server_status)
-print("Server Time is " + server_time)
-print("")
-print("")
+def get_BIserver_status():
+    server_status = client.get_system_status()
+    server_status = server_status["msg"]
+    server_time = client.get_server_time()
+    server_time = server_time["serverTime"]
+    server_time = datetime.datetime.fromtimestamp(server_time/1000)
+    server_time = server_time.replace(second=0, microsecond=0)
+    server_time = server_time.strftime("%H:%M %d-%m-%Y")
+    print("")
+    print("Server status is " + server_status)
+    print("Server Time is " + server_time)
+    print("")
+
+get_BIserver_status()
