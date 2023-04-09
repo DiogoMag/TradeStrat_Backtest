7caeee615b8ba0fef25b50d42b866542e34e9d35
DiogoMag <diogor.magalhaes@gmail.com>
2023-04-09 20:59:45 +0100

    fixed GetAccount_info

--- Experiment/AccountBalance/GetAccount_Info.py
+++ Experiment/AccountBalance/GetAccount_Info.py
@@ -11,11 +11,31 @@ test_api_key = os.environ.get('TEST_API')
 test_api_secret = os.environ.get('TEST_SAPI')
 client = Client(test_api_key, test_api_secret, testnet=True)
 
-## Define asset for analysis
-asset = 'BTC'
 
-def get_account_balance(ticker):
-    print('1')
+def getAccount_balance(symbol):
+    if symbol == 'all':
+        #Account wallet
+        account_wallet = client.get_account()
+        account_wallet = pd.DataFrame(account_wallet['balances'], columns=['asset', 'free', 'locked'])
+        account_wallet.columns = ["Asset","Free","Locked"]
+        account_wallet[["Free","Locked"]] = account_wallet[["Free","Locked"]].astype(float)
+        account_wallet[['Free', 'Locked']] = account_wallet[['Free', 'Locked']].round(4)
+        pd.set_option('display.float_format', '{:.4f}'.format)
 
 
-get_account_balance('all')
+        return account_wallet
+
+    else:
+        # Get balance for the specified asset
+        balance = client.get_asset_balance(asset=symbol)
+        balanceFree = balance['free']
+        balanceLocked = balance['locked']
+
+        return balance
+
+
+
+
+## test function
+print(getAccount_balance('BTC'))
+print(getAccount_balance('all'))
