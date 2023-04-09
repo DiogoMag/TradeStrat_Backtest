f639a100ed985398601393ea5d260f8a82d494ae
DiogoMag <diogor.magalhaes@gmail.com>
2023-04-09 22:02:06 +0100

    UI Testing in MAIN.py

--- MAIN/AccountBalance/GetAccount_Info.py
+++ MAIN/AccountBalance/GetAccount_Info.py
@@ -2,6 +2,18 @@ from binance import Client
 import pandas as pd
 import os
 
+##__________ This script contains the following functions: __________
+
+#       - getAcc_balance()
+#               1 parameter, Example: 'all' / 'BTC' / 'ETH' etc
+
+#       - get_openOrders()
+#               1 parameter Example: 'all' / 'BTC' / 'ETH' etc
+
+#       - get_allOrders() - gets open, closed and canceled orders
+#                no parameters
+
+#       - getAccount_balance
 
 ## Clear screen
 os.system('cls')
@@ -11,10 +23,9 @@ test_api_key = os.environ.get('TEST_API')
 test_api_secret = os.environ.get('TEST_SAPI')
 client = Client(test_api_key, test_api_secret, testnet=True)
 
-
-def getAccount_balance(symbol):
+#       - getAccount_balance() - parameter Example: 'all' or 'BTC'
+def getAcc_balance(symbol):
     if symbol == 'all':
-        #Account wallet
         account_wallet = client.get_account()
         account_wallet = pd.DataFrame(account_wallet['balances'], columns=['asset', 'free', 'locked'])
         account_wallet.columns = ["Asset","Free","Locked"]
@@ -25,7 +36,7 @@ def getAccount_balance(symbol):
         return account_wallet
 
     else:
-        account_wallet = getAccount_balance('all') # Get account wallet
+        account_wallet = getAcc_balance('all')
         if symbol in account_wallet['Asset'].values:
             balance = client.get_asset_balance(asset=symbol)
 
