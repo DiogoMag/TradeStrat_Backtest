0fcb35ca767a8a0a5b3325ae0373a65007f1b12d
DiogoMag <diogor.magalhaes@gmail.com>
2023-04-10 02:25:00 +0100

    Fixed: find_marketsession in price action= working

--- Indicators/FindPriceAction/find_PriceAction.py
+++ Indicators/FindPriceAction/find_PriceAction.py
@@ -1,34 +1,51 @@
 import datetime
+import pytz
+import os
 
-now = datetime.datetime.now().time()
 
-
-# _________________________________________________________ FUNCTIONS #
+os.system('cls')
 
 def find_marketsession(stamp):
-    # Convert timestamp to datetime object
-    dt = datetime.datetime.fromtimestamp(stamp)
-    now = dt.time()
+    
+    # Convert timestamp to datetime object in UTC
+    dt_utc = datetime.datetime.utcfromtimestamp(stamp)
 
-    # Define session start and end times in GMT
-    tokyo_start = datetime.time(0, 0)
-    tokyo_end = datetime.time(9, 0)
+    # Convert UTC datetime object to London timezone datetime object
+    london_tz = pytz.timezone('Europe/London')
+    dt_london = dt_utc.replace(tzinfo=pytz.utc).astimezone(london_tz)
 
-    london_start = datetime.time(8, 0)
-    london_end = datetime.time(17, 0)
+    # Define session start and end times in London timezone
 
     newyork_start = datetime.time(13, 0)
     newyork_end = datetime.time(22, 0)
 
-    # Identify the current session
-    if tokyo_start <= now < tokyo_end:
-        return "Tokyo session"
-    elif london_start <= now < london_end:
-        return "London session"
-    elif newyork_start <= now < newyork_end:
-        return "New York session"
-    else:
-        return "Gap or dead zone"
+    london_start = datetime.time(8, 0)
+    london_end = datetime.time(17, 0)
+
+    asia_start = datetime.time(1, 0)
+    asia_end = datetime.time(9, 0)
+
+    sidney_start = datetime.time(22, 0)
+    sidney_end = datetime.time(7, 0)
+
+    deadzone_start = datetime.time(22, 0)
+    deadzone_end = datetime.time(1, 0)
+
+    # Determine which market sessions the timestamp falls within
+    session_list = []
+    if newyork_start <= dt_london.time() <= newyork_end:
+        session_list.append("New York")
+    if london_start <= dt_london.time() <= london_end:
+        session_list.append("London")
+    if asia_start <= dt_london.time() <= asia_end:
+        session_list.append("Asia")
+    if sidney_start <= dt_london.time() <= datetime.time(23, 59, 59) or dt_london.time() <= sidney_end:
+        session_list.append("Sydney")
+    if deadzone_start <= dt_london.time() <= datetime.time(23, 59, 59) or dt_london.time() <= deadzone_end:
+        session_list.append("Dead Zone")
+
+    return session_list
+
 
 
 def find_reversal():
